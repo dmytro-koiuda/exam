21464ab9791f74766317e863c1c4ba9c4cef0b5e
dmytro-koiuda <168765160+dmytro-koiuda@users.noreply.github.com>
2024-06-04 09:15:08 +0200

    Initial commit

---
+++ src/main/java/edu/hneu/mjt/koiuda/ticket_1_1/Ticket11Application.java
@@ -0,0 +1,13 @@
+package edu.hneu.mjt.koiuda.ticket_1_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ticket11Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ticket11Application.class, args);
+    }
+
+}
---
+++ src/test/java/edu/hneu/mjt/koiuda/ticket_1_1/Ticket11ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.hneu.mjt.koiuda.ticket_1_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ticket11ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
